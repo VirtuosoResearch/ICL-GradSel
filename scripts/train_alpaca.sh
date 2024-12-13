5ba203ca5f07d6f0cae75330140e44d7f18f1618
MichaelZona <2462970640@qq.com>
2024-12-13 13:30:17 -0500

    update

--- scripts/train_alpaca.sh
+++ scripts/train_alpaca.sh
@@ -3,6 +3,7 @@ sv="fast_approximation_gradients_double_adapter_quant2"
 bs=8
 dev=1
 rf=128
+epochs=20
 # "EleutherAI/gpt-j-6b"
 # "meta-llama/Llama-3.2-1B"
 # "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T"
@@ -16,4 +17,4 @@ python train_alpaca.py \
     --batch_size $bs --max_length 256 \
     --devices $dev --strategy auto --compute_pretrained_outputs --save_name $sv\
     --downsample 1000 --num_batches_gradients 1000\
-    --train_adapter --reduction_factor $rf --epochs 50 --loss_file "loss_1k_50_00005.log" --num_samples 20 --lr 5e-5
+    --train_adapter --reduction_factor $rf --epochs $epochs --loss_file "loss_1k_50_00005.log" --num_samples 20 --lr 5e-5
