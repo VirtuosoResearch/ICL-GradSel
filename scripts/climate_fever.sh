15ef6c7801caabf0192c6c7414455b8017d543a8
MichaelZona <2462970640@qq.com>
2025-01-13 19:35:01 -0500

    update

---
+++ scripts/climate_fever.sh
@@ -0,0 +1,11 @@
+
+
+python test.py --dataset climate_fever --gpt2 gpt2-large --method direct --out_dir out/gpt2-large --do_zeroshot --test_batch_size 4 --use_demonstrations  --seed 100 --k 1 --topk
+
+python test.py --dataset climate_fever --gpt2 gpt2-large --method direct --out_dir out/gpt2-large --do_zeroshot --test_batch_size 4 --use_demonstrations  --seed 100 --k 2 --topk
+
+python test.py --dataset climate_fever --gpt2 gpt2-large --method direct --out_dir out/gpt2-large --do_zeroshot --test_batch_size 4 --use_demonstrations  --seed 100 --k 1 --randomk
+
+python test.py --dataset climate_fever --gpt2 gpt2-large --method direct --out_dir out/gpt2-large --do_zeroshot --test_batch_size 4 --use_demonstrations  --seed 100 --k 2 --randomk
+
+
