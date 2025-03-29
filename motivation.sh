0b21a55a7add9c67182baa5cd589075b21df6156
MichaelZona <2462970640@qq.com>
2025-03-28 23:00:47 -0400

    update

--- motivation.sh
+++ motivation.sh
@@ -1,4 +1,26 @@
-python motivation.py --task poem_sentiment
-python motivation.py --task climate_fever
-python motivation.py --task medical_questions_pairs
-python motivation.py --task glue-mrpc
+
+# python motivation.py --task poem_sentiment --k 2 --seed 4 --model facebook/opt-1.3b
+# python motivation.py --task poem_sentiment --k 2 --seed 6 --model facebook/opt-1.3b
+# python motivation.py --task poem_sentiment --k 2 --seed 2 --model facebook/opt-1.3b
+
+# python motivation.py --task poem_sentiment --k 4 --seed 4 --model facebook/opt-1.3b
+# python motivation.py --task poem_sentiment --k 4 --seed 6 --model facebook/opt-1.3b
+# python motivation.py --task poem_sentiment --k 4 --seed 2 --model facebook/opt-1.3b
+ 
+# python motivation.py --task poem_sentiment --k 6 --seed 4 --model facebook/opt-1.3b
+# python motivation.py --task poem_sentiment --k 6 --seed 6 --model facebook/opt-1.3b
+# python motivation.py --task poem_sentiment --k 6 --seed 2 --model facebook/opt-1.3b
+
+
+# python motivation.py --task poem_sentiment --k 8 --seed 4 --model facebook/opt-1.3b
+# python motivation.py --task poem_sentiment --k 8 --seed 6 --model facebook/opt-1.3b
+# python motivation.py --task poem_sentiment --k 8 --seed 2 --model facebook/opt-1.3b
+
+
+# python motivation.py --task poem_sentiment --k 10 --seed 4 --model facebook/opt-1.3b
+# python motivation.py --task poem_sentiment --k 10 --seed 6 --model facebook/opt-1.3b
+# python motivation.py --task poem_sentiment --k 10 --seed 2 --model facebook/opt-1.3b
+
+python motivation.py --task poem_sentiment --k 2 --model meta-llama/Llama-3.1-8B
+# python motivation.py --task superglue-cb --model meta-llama/Llama-3.1-8B
+# python motivation.py --task medical_questions_pairs --model meta-llama/Llama-3.1-8B
