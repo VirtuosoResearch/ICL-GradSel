ab5840bab3e4307522d5f9e7358b457a8cab7cd8
zhenshuozhang <zs.zhang0120@gmail.com>
2025-08-25 23:10:53 -0400

    scripts

--- examples/gradce.sh
+++ examples/gradce.sh
@@ -1,1 +1,1 @@
-python test.py --dataset sst2 --gpt2 deepseek-ai/deepseek-llm-7b-chat --method direct --do_zeroshot --test_batch_size 4 --use_demonstrations  --seed 0 --k 8 --groundestim
+python test.py --dataset sst2 --model deepseek-ai/deepseek-llm-7b-chat --method direct --do_zeroshot --test_batch_size 4 --use_demonstrations  --seed 0 --k 8 --groundestim
--- examples/ranens.sh
+++ examples/ranens.sh
@@ -1,3 +1,3 @@
-python test.py --dataset sst2 --gpt2 deepseek-ai/deepseek-llm-7b-chat --method direct --do_zeroshot --test_batch_size 4 --use_demonstrations  --seed 0 --k 8 --ranens
+python test.py --dataset sst2 --model deepseek-ai/deepseek-llm-7b-chat --method direct --do_zeroshot --test_batch_size 4 --use_demonstrations  --seed 0 --k 8 --ranens
 
-python test.py --dataset sst2 --gpt2 meta-llama/Llama-3.2-3B-Instruct --method direct --do_zeroshot --test_batch_size 4 --use_demonstrations  --seed 0 --k 8 --ranens
+python test.py --dataset sst2 --model meta-llama/Llama-3.2-3B-Instruct --method direct --do_zeroshot --test_batch_size 4 --use_demonstrations  --seed 0 --k 8 --ranens
